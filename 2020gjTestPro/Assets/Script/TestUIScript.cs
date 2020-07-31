fde8fd9385dbf534b8568c79d5591c13ca3abdd1
1375161442 <1375161442@qq.com>
2020-07-31 14:18:17 +0800

    新增测试代码和ui

---
+++ 2020gjTestPro/Assets/Script/TestUIScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestUIScript : MonoBehaviour
+{
+    public GameObject Image = null;
+    public GameObject CtrlButton = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ChangeShowImage(bool isShow)
+    {
+        if (isShow)
+        {
+            CtrlButton.SetActive(false);
+            Image.SetActive(true);
+        }
+        else
+        {
+            CtrlButton.SetActive(true);
+            Image.SetActive(false);
+        }
+    }
+}
